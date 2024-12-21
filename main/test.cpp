fc7720b8a6d9d2b373ec7382a5736c78b36bbf2b
ieRiku <mriganka9432@gmail.com>
2024-12-21 04:49:32 +0000

    made changes to test.cpp

--- main/test.cpp
+++ main/test.cpp
@@ -4,16 +4,16 @@ using namespace std;
 extern void decompressSequence(string);
 extern void compressSequence(string);
 extern void normalize(int argc, char* argv[]);
-extern void denormalize(int argc, char* argv[]);
+//extern void denormalize(int argc, char* argv[]);
 
 
 int main(int argc, char* argv[]){
     string str = argv[1];
     string file = getFileNameWithoutExtension(str);
     normalize(2, argv);
-    string inputFilePath = "../dna/norm/" + file + ".txt";
+    string inputFilePath = "../dna/norm/" + file + "_raw.txt";
     compressSequence(inputFilePath);
     decompressSequence(inputFilePath);
-    denormalize(2, argv);
+    //denormalize(2, argv);
     return 0;
 }
